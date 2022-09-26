45e07458f43188a36d0f4abb0c7c0791cb3b4f40
StrJosedavi <josedavi100@gmail.com>
2022-09-25 21:08:53 -0300

    iniciando projeto react

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+
+export default function App() {
+  return (
+    <div>
+      <h1>Hello World!</h1>
+      <h2>Olá Mundo</h2>
+    </div>
+  );
+}
